980d80a02ef75ce2c7ca5ebc79329dd2e93c030f
Yusuke Kawasaki <u-suke@kawa.net>
2018-08-08 23:02:32 +0900

    parser.ts is just a parser

--- src/parser.ts
+++ src/parser.ts
@@ -25,63 +25,56 @@ const access = promisen.denodeify(fs.access.bind(fs));
 
 export module Parser
 {
-	export function parse(names?: string[]) {
-		const mesh1 = {};
-		const mesh2 = {};
-		const cities = {};
+	const meshIndex = {};
+	const nameIndex = {};
+	let initialized;
 
-		return readCSV(names).then(array => {
+	export function init() {
+		if (initialized) return promisen.resolve();
+
+		return readCSV(NAMES).then(array => {
 			array.forEach(row => {
-				// header row
+				// ignore header row
 				if (!(+row[0])) return;
 
 				const city = row[0];
-				const code = row[2];
-				const code2 = code.substr(0, 6);
-				const code3 = code.substr(6);
-
-				const idx2 = mesh2[code2] || (mesh2[code2] = {});
-				idx2[city] = (idx2[city] || 0) + 1;
-
-				const idx1 = mesh1[code2] || (mesh1[code2] = {});
-				const list3 = idx1[code3] || (idx1[code3] = {});
-				list3[+city] = 1;
+				const array = meshIndex[city] || (meshIndex[city] = []);
+				array.push(row[2]);
 
 				// city code -> name
-				if (!cities[city]) {
-					cities[city] = row[1].replace(/^.+(支庁|(総合)?振興局)/, "");
+				if (!nameIndex[city]) {
+					nameIndex[city] = row[1].replace(/^.+(支庁|(総合)?振興局)/, "");
 				}
 			});
-		}).then(() => {
-			const mesh = {};
 
-			Object.keys(mesh2).forEach(code2 => {
-				const idx2 = mesh2[code2];
-				const array = Object.keys(idx2).sort((a, b) => {
-					return ((idx2[b] - idx2[a]) || ((+b) - (+a)));
-				});
-				const city = mesh2[code2] = +array[0];
-
-				const single = {};
-				const multi = {};
-				const item: Array<number | object> = mesh[code2] = [city];
-				if (array.length < 2) return;
-
-				item.push(single, multi);
-
-				const idx1 = mesh1[code2];
-				Object.keys(idx1).forEach(code3 => {
-					const list3 = Object.keys(idx1[code3]).map(v => +v);
-					if (list3.length > 1) {
-						multi[code3] = list3;
-					} else if (list3[0] !== city) {
-						single[code3] = list3[0];
-					}
-				});
-			});
-
-			return {mesh: mesh, city: cities};
+			initialized = true;
 		});
+	}
+
+	/**
+	 * @return {string[]} array of JIS city codes
+	 */
+
+	export function all(): string[] {
+		return Object.keys(nameIndex).sort();
+	}
+
+	/**
+	 * @param {string} city - JIS city code
+	 * @return {string[]} array of mesh codes for the city
+	 */
+
+	export function mesh(city: string): string[] {
+		return meshIndex[city];
+	}
+
+	/**
+	 * @param {string} city - JIS city code
+	 * @return {string} name of the city
+	 */
+
+	export function name(city: string): string {
+		return nameIndex[city];
 	}
 
 	/**
@@ -90,9 +83,6 @@ export module Parser
 
 	function readCSV(names) {
 		let all = [];
-
-		// read all files when the argument not given
-		if (!names) names = NAMES;
 
 		return promisen.eachSeries(names, it)().then(() => all);
 
--- src/prepare.ts
+++ src/prepare.ts
@@ -9,15 +9,62 @@ const MESH = BASE_DIR + "dist/mesh.json";
 const CITY = BASE_DIR + "dist/city.json";
 
 function CLI() {
-	Parser.parse().then(data => {
+	return Parser.init().then(() => {
+		const mesh1 = {};
+		const mesh2 = {};
+		const names = {};
+
+		Parser.all().forEach(city => {
+			names[city] = Parser.name(city);
+
+			Parser.mesh(city).forEach(code => {
+				const code2 = code.substr(0, 6);
+				const code3 = code.substr(6);
+
+				const idx2 = mesh2[code2] || (mesh2[code2] = {});
+				idx2[city] = (idx2[city] || 0) + 1;
+
+				const idx1 = mesh1[code2] || (mesh1[code2] = {});
+				const list3 = idx1[code3] || (idx1[code3] = {});
+				list3[+city] = 1;
+			});
+		});
+
+		const mesh = {};
+
+		Object.keys(mesh2).forEach(code2 => {
+			const idx2 = mesh2[code2];
+			const array = Object.keys(idx2).sort((a, b) => {
+				return ((idx2[b] - idx2[a]) || ((+b) - (+a)));
+			});
+			const city = mesh2[code2] = +array[0];
+
+			const item: Array<number | object> = mesh[code2] = [city];
+			if (array.length < 2) return;
+
+			const single = {};
+			const multi = {};
+			item.push(single, multi);
+
+			const idx1 = mesh1[code2];
+			Object.keys(idx1).forEach(code3 => {
+				const list3 = Object.keys(idx1[code3]).map(v => +v);
+				if (list3.length > 1) {
+					multi[code3] = list3;
+				} else if (list3[0] !== city) {
+					single[code3] = list3[0];
+				}
+			});
+		});
+
 		let json;
 
-		json = JSON.stringify({mesh: data.mesh});
+		json = JSON.stringify({mesh: mesh});
 		json = json.replace(/("\d{6}":)/g, "\n$1");
 		Logger.log("writing:", MESH);
 		fs.writeFileSync(MESH, json);
 
-		json = JSON.stringify({city: data.city});
+		json = JSON.stringify({city: names});
 		json = json.replace(/("\d+":)/g, "\n$1");
 		Logger.log("writing:", CITY);
 		fs.writeFileSync(CITY, json);
