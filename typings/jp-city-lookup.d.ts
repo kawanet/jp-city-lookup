9e077ac54b2079ec86e585f5aac7b56ad110235a
Yusuke Kawasaki <u-suke@kawa.net>
2018-10-02 17:29:18 +0900

    npm run tsc-typings

--- typings/jp-city-lookup.d.ts
+++ typings/jp-city-lookup.d.ts
@@ -1,11 +1,16 @@
-export declare module City {
-    function name(code: string | number): string;
-    function lookup(options: {
-        pref?: string | number;
-        ll?: string;
-        lat?: number;
-        lng?: number;
-        mesh?: string;
-        neighboring?: string | number;
-    }): string[];
+type MeshCode = string;
+type PrefCode = string;
+type CityCode = string;
+export interface CityOptions {
+    pref?: PrefCode | number;
+    ll?: string;
+    lat?: number;
+    lng?: number;
+    mesh?: MeshCode;
+    neighboring?: CityCode | number;
 }
+export declare module City {
+    function name(code: CityCode | number): string;
+    function lookup(options: CityOptions): CityCode[];
+}
+export {};
