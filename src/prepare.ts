5e6ded9ca1412dfe21654f3f3a9fa3fcf44a16af
Yusuke Kawasaki <u-suke@kawa.net>
2018-10-02 17:14:07 +0900

    remove forEach()

--- src/prepare.ts
+++ src/prepare.ts
@@ -25,7 +25,9 @@ function CLI(meshJson: string, cityJson: string) {
     const meshIndex = {} as MeshCityMaster;
     const nameIndex = {} as CityNameMaster;
 
-    files.forEach(name => {
+    // STEP #1
+
+    for (const name of files) {
         const file = "./node_modules/jp-data-mesh-csv/" + name;
         WARN("reading: " + file);
 
@@ -35,9 +37,11 @@ function CLI(meshJson: string, cityJson: string) {
 
         const rows = data.split(/\r?\n/).map(line => line.split(",").map(col => col.replace(/^"(.*)"$/, "$1")));
 
-        rows.forEach(([city, name, code]) => {
-            if (!+city) return;
-            if (name.search(/境界未定/) > -1) return;
+        for (const row of rows) {
+            const [city, name, code] = row;
+
+            if (!+city) continue;
+            if (name.search(/境界未定/) > -1) continue;
 
             const code2 = code.substr(0, 6);
             const code3 = code.substr(6);
@@ -52,41 +56,52 @@ function CLI(meshJson: string, cityJson: string) {
             if (!nameIndex[city]) {
                 nameIndex[city] = name.replace(/^.+(支庁|(総合)?振興局)/, "");
             }
-        });
-    });
+        }
+    }
 
-    const list2 = Object.keys(indexB2);
-    list2.forEach(code2 => {
+    // STEP #2
+
+    for (const code2 in indexB2) {
         const indexB1 = indexB2[code2] as HitIndex1;
-        const array = Object.keys(indexB1).sort((a: CityCode, b: CityCode) => {
-            return (((+indexB1[b]) - (+indexB1[a])) || ((+b) - (+a)));
-        });
+        const sorter = (a: CityCode, b: CityCode) => (((+indexB1[b]) - (+indexB1[a])) || ((+b) - (+a)));
+        const array = Object.keys(indexB1).sort(sorter);
+
+        // the most major city in the level 2 mesh
         const city = +array[0];
-
         const item = meshIndex[code2] = [city] as MeshCityItem;
-        if (array.length < 2) return;
 
+        // the level 2 mesh has just one city
+        if (array.length < 2) continue;
+
+        // the level 2 mesh has multiple cities
         const single = {} as SingleCityMesh;
         const multi = {} as MultipleCityMesh;
         item.push(single, multi);
 
         const indexA2 = indexA3[code2] as HitIndex2;
-        Object.keys(indexA2).forEach(code3 => {
+        for (const code3 in indexA2) {
             const list3 = Object.keys(indexA2[code3]).map(v => +v);
+
             if (list3.length > 1) {
+                // the level 3 mesh has multiple cities
                 multi[code3] = list3;
             } else if (list3[0] !== city) {
+                // the level 3 mesh has just one city
                 single[code3] = list3[0];
             }
-        });
-    });
+        }
+    }
+
+    // STEP #3
 
     let json;
 
+    // mesh.json
     json = JSON.stringify({mesh: meshIndex});
     json = json.replace(/("\d{6}":)/g, "\n$1");
     write(meshJson, json);
 
+    // city.json
     json = JSON.stringify({city: nameIndex});
     json = json.replace(/("\d+":)/g, "\n$1");
     write(cityJson, json);
