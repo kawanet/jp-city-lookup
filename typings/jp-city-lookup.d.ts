9d1687087088571ae9a42b36b98c51637b964af0
Yusuke Kawasaki <u-suke@kawa.net>
2021-05-07 20:10:10 +0900

    npm run tsc-types removed

--- typings/jp-city-lookup.d.ts
+++ typings/jp-city-lookup.d.ts
@@ -1,16 +1,35 @@
+/**
+ * jp-city-lookup
+ *
+ * @see https://github.com/kawanet/jp-city-lookup
+ */
+
 type MeshCode = string;
 type PrefCode = string;
 type CityCode = string;
+
 export interface LookupOptions {
-    pref?: PrefCode | number;
-    ll?: string;
-    lat?: number;
-    lng?: number;
-    mesh?: MeshCode;
-    neighboring?: CityCode | number;
+    /// JIS prefecture code
+    pref?: PrefCode | number,
+
+    /// latitude,longitude
+    ll?: string,
+
+    /// latitude
+    lat?: number,
+
+    /// longitude
+    lng?: number,
+
+    /// mesh code
+    mesh?: MeshCode,
+
+    /// JIS city code
+    neighboring?: CityCode | number,
 }
+
 export declare module City {
     function name(code: CityCode | number): string | undefined;
+
     function lookup(options: LookupOptions): CityCode[];
 }
-export {};
